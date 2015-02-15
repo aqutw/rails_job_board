e0e89bebefaa3920f274a8d20dd8cbe5016e2430
aqutw <aquarian.ex@gmail.com>
2015-02-15 21:49:29 +0800

    rails g migration add_category_id_to_jobs category_id:integer && rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215134704) do
+ActiveRecord::Schema.define(version: 20150215134901) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150215134704) do
     t.string   "url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
 end
