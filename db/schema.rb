8f3be53c3deefeea8e94a9e20cab9559bea90d15
aqutw <aquarian.ex@gmail.com>
2015-02-15 21:47:59 +0800

    rails g model category name:string && rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215130944) do
+ActiveRecord::Schema.define(version: 20150215134704) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "jobs", force: true do |t|
     t.string   "title"
